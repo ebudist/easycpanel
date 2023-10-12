ce6c075b9351935a7dcc16caaa38124316e9b6d7
ebudist <59221512+ebudist@users.noreply.github.com>
2023-10-13 02:10:28 +0700

    Update optimize-apache.sh
    Add fixed NEWPORTNUM for standarized 8435 port (Commented)

--- optimize-apache.sh
+++ optimize-apache.sh
@@ -231,6 +231,8 @@ perl -pi -e 's/LF_HTACCESS_PERM = "1"/LF_HTACCESS_PERM = "1800"/g' /etc/csf/csf.
 perl -pi -e 's/LF_MODSEC_PERM = "1"/LF_MODSEC_PERM = "1800"/g' /etc/csf/csf.conf ;
 CURRENTPORTNUM=22
 NEWPORTNUM=$(( $RANDOM % 500  + 1500 ))
+## Use this instead for fixed port number 8435 (Talk to L3 tech for information) ###
+#NEWPORTNUM=8435
 TCPIN=$(cat /etc/csf/csf.conf | grep ^TCP_IN)
 TCPINNEW=$(cat /etc/csf/csf.conf | grep ^TCP_IN | sed -e "s/,${CURRENTPORTNUM},/,${NEWPORTNUM},/")
 sed -i "s/$TCPIN/$TCPINNEW/g" /etc/csf/csf.conf
